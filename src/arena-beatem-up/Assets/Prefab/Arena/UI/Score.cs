278e9f75caf6685a0dc7f1d111224d11b5defcd5
Danny Lewis <nitomatik@gmail.com>
2019-06-30 13:18:07 -0600

    Adding score to top right with tester button for the interim

---
+++ src/arena-beatem-up/Assets/Prefab/Arena/UI/Score.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public string ScorePrefix;
+    
+    private Text _scoreText;
+    private int _scoreAmount;
+
+    public static Score Instance { get; private set; }
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _scoreText = GetComponent<Text>();
+        _scoreAmount = 0;
+        _scoreText.text = ScorePrefix + _scoreAmount;
+    }
+
+    // Adds to the score being displayed
+    public void AddScore(int toAdd)
+    {
+        _scoreAmount += toAdd;
+        _scoreText.text = ScorePrefix + _scoreAmount;
+    }
+}
